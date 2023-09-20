c9fb2d96f6de683d4947beb29e3df122d99518c7
Ian G <56713583+pseudonoma@users.noreply.github.com>
2023-09-21 08:50:43 +1000

    Moved do_fluxxer.sh
    It's in /dev/ now, and a cleaner simple script (testrun_fluxxer.sh) should be used for the time being if the data needs to be reanalyzed.

--- dev/dev_do_fluxxer.sh
+++ dev/dev_do_fluxxer.sh
@@ -9,10 +9,6 @@ sourceDir=./data/wrangled
 
 
 # target code:
-Rscript ./R/fluxxer.R -i "$sourceDir/FLUCTEST 1 2020 09 24_short_unpooled.csv" -o "FLUCTEST1_short_unpooled" -c
-Rscript ./R/fluxxer.R -i "$sourceDir/FLUCTEST 1 2020 09 24_short_pooled.csv" -o "FLUCTEST1_short_pooled"
-Rscript ./R/fluxxer.R -i "$sourceDir/FLUCTEST 1 2020 09 24_long_unpooled.csv" -o "FLUCTEST1_long_unpooled" -c
-Rscript ./R/fluxxer.R -i "$sourceDir/FLUCTEST 1 2020 09 24_long_pooled.csv" -o "FLUCTEST1_long_pooled"
 Rscript ./R/fluxxer.R -i "$sourceDir/FLUCTEST 2 2023 02 06_unpooled.csv" -o "FLUCTEST2_unpooled" -c
 Rscript ./R/fluxxer.R -i "$sourceDir/FLUCTEST 2 2023 02 06_pooled.csv" -o "FLUCTEST2_pooled"
 
