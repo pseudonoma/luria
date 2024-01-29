84646259b28abcda8cb4b82640a1305920086a31
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-01-29 16:16:30 +1000

    sync

--- R/dev_notes.R
+++ R/dev_notes.R
@@ -2,16 +2,24 @@
 #
 ###
 
+# Current to-do
+# Rewrite rawfile wrangler
+#   Put in dir setup
+
+
 # Pipeline autofunction
 # auto_flux() - Might need this to automatically decide if func 1-1 or 1-2 is required. Also not
 #    clear if this is where dir structure should be handled. If core functions are designed to be
 #    called by the user, then the wrapper can't be relied on to handle dir checking. If 
 
-# Part 1 - Wrangling
-# 1. wrangle_raw()
-# 2. wrangle_intermediate()
-# Additional req: 
-#  dir structure must be completed here; refuse to run Part 2 if dir structure not recognized
+# Part 1 - Wrangling [DONE]
+# wrangle_raw_data()
+# wrangle_clean_data()
+# Note: 
+# These functions don't interact with each other; both of them (a) make the same pooled/unpooled 
+# "clean" files for passing to Part 2, and (b) enforce the standard dir structure that Part 2 
+# looks for before it continues. 
+# Part 2 must not run independently or out of sequence.
 
 # Part 2 - Analysis
 # run_fluxxer()
