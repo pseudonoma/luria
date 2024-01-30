5619e91c8a298c53e76c4e834cc9fbc197d87d9f
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-01-30 17:16:38 +1000

    Rewrote run_fluxxer.R
    Function now has two modes, "single", and "standard".
    
    "Single" is mostly a minimal wrapper for calculate_mut_rate() with an additional check that supplied file is not a folder. "Standard" assumes pipeline dir structure and pooled/unpooled data.
    
    Removed arg dataPath, internal c_m_r() call for standard mode now parses filename from inputPath, which is hardcoded to follow the pipeline dirs. outputPath is also hardcoded the same way.
    
    Added arg "comparisons" for use with single mode, where it's passed to c_m_r(). Standard mode will ignore it. Not sure how I feel about its utility but I'm moving on for now.
    
    Both modes use prep_export() and export to ./output/analyzed just to make my life easier.

--- R/run_fluxxer.R
+++ R/run_fluxxer.R
@@ -2,48 +2,67 @@
 # Wrapper function that neatly calls calculateMutRate() on all files in /wrangled/.
 # Also sets up some standard pathing etc. before calling calculateMutRate().
 
-run_fluxxer <- function(dataPath = NULL, poolAs = NULL){
+run_fluxxer <- function(file = NULL, comparisons = TRUE){
   
   # ARGS:
-  # dataPath - core data folder containing /raws/, /wrangled/, etc.
-  # poolAs - What to name the pooled strain. Defaults to "Combined".
+  # filePath - the individual file to run on; will trigger "single" mode if anything supplied.
+  # comparisons - logical, defaults to TRUE for use in "single" mode cmr() call. In "standard"
+  #               mode it does nothing. Not sure if this is actually useful but whatever man.
   
   ###
   
-  # Check for previous step in the pipeline
-  inputPath <- paste0(dataPath, "/wrangled")
-  if(!dir.exists(paste0(dataPath, "/wrangled"))){
-    stop("Folder /wrangled/ expected but not found, did you run the pipeline correctly?")
-  }
+  # Set mode based on whether individual file is supplied
+  runMode <- ifelse(is.null(file), yes = "standard", no = "single")
   
-  # Create output folder
+  # Create standard output folders regardless of mode
   prep_export(mode = "analyzed")
-  # outputPath <- paste0(dataPath, "/analyzed")
-  # if(dir.exists(outputPath)){
-  #   warning("The output folder /analyzed/ already exists - files may have been overwritten.")
-  # } else {
-  #   dir.create(outputPath, showWarnings = FALSE)
-  # }
   
-  # Define pooled/unpooled filenames
-  pooledList <- grep("_pooled", dir(inputPath), value = TRUE)
-  unpooledList <- grep("_unpooled", dir(inputPath), value = TRUE)
+  ##### Single-file mode #####
+  if(runMode == "single"){
+    
+    # test for file validity
+    if(file_test("-d", file)){
+      stop("Supplied file appears to be a folder. If running the standard pipeline, do not supply a filename.")
+    }
+    
+    # run core fluxxer function
+    calculateMutRate(filename = file,
+                     outputPath = , 
+                     output_prefix = baseName, 
+                     comparisons = comparisons)
+  } # end single mode
   
-  # Run core fluxxer.R function on pooled, then unpooled, data
-  for(file in pooledList){
-    baseName <- sub(".csv", "", file)
-    calculateMutRate(dataPath,
-                     filename = paste0(dataPath, "/wrangled/", file), 
-                     output_prefix = baseName, 
-                     comparisons = FALSE)
-  }
-  for(file in unpooledList){
-    baseName <- sub(".csv", "", file)
-    calculateMutRate(dataPath,
-                     filename = paste0(dataPath, "/wrangled/", file), 
-                     output_prefix = baseName, 
-                     comparisons = TRUE)
-  }
+  ##### Standard pipeline mode #####
+  if(runMode == "standard"){
+    
+    # Test for standard pipeline dirs
+    inputPath <- "./output/wrangled"
+    if(!dir.exists(inputPath)){
+      stop("Folder /wrangled/ not found. In standard mode, the pipeline must be run in order.")
+    }
+    
+    # Define pooled/unpooled filenames
+    pooledList <- grep("_pooled", dir(inputPath), value = TRUE)
+    unpooledList <- grep("_unpooled", dir(inputPath), value = TRUE)
+    
+    # Run core fluxxer function on pooled, then unpooled, data
+    for(file in pooledList){
+      baseName <- sub(".csv", "", file)
+      calculate_mut_rate(filename = paste0(inputPath, "/", file),
+                         outputPath = "./output/analyzed",
+                         outputPrefix = baseName, 
+                         comparisons = FALSE)
+    }
+    for(file in unpooledList){
+      baseName <- sub(".csv", "", file)
+      calculate_mut_rate(filename = paste0(inputPath, "/", file),
+                         outputPath = "./output/analyzed",
+                         outputPrefix = baseName, 
+                         comparisons = TRUE)
+    }
+    
+  } # end standard mode
+ 
   
   message("\nDone. Check /analyzed/ for outputs.\n")
   
