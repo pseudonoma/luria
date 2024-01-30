99c58c1ee9b703c5ecf47b6ff5acc2fa98b67942
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-01-30 16:11:19 +1000

    Made dev test script
    Just a script for testing the new luria functions in development.

--- dev/test_300124.R
+++ dev/test_300124.R
@@ -9,3 +9,7 @@ source("./R/wrangle_clean_data.R")
 testRaw <- ("./data/testing 2024 01 30/test_raw.xlsx")
 testWrangled <- ("./data/testing 2024 01 30/test_wrangled.csv")
 
+###
+unlink("./output", recursive = TRUE) # remove /output/ since there's a stop() call
+wrangle_raw_data(testRaw, 4)
+wrangle_clean_data(testWrangled)
