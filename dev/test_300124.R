157c8f1160e175b09340982963b6ce9c504cfa4f
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-01-30 13:37:58 +1000

    Cleaned up /dev/
    I don't know what happened here but I couldn't keep track of what I was doing. The files have all been moved into an "old" folder. Also set up some testing files based on FLUCTEST1 and E.L.'s L. plantarum data.

---
+++ dev/test_300124.R
@@ -0,0 +1,11 @@
+# testbed 30/01/24
+# for testing the new luria pipeline, with standardized dir structure
+
+###
+
+source("./R/helpers.R")
+source("./R/wrangle_raw_data.R")
+source("./R/wrangle_clean_data.R")
+testRaw <- ("./data/testing 2024 01 30/test_raw.xlsx")
+testWrangled <- ("./data/testing 2024 01 30/test_wrangled.csv")
+
