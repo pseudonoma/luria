2d2ae856beb591558d4efc25f1fcb2165d35ea0b
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-02-01 15:59:05 +1000

    Changed plot_mutrates()
    New implementation of the annotation_logticks call.
    
    Because annotation_logticks shits itself if the upper and lower CIs span a whole power of 10, it's now conditionally called outside the initial ggplot2 call, depending on whether wrangle_plot_data() thought annotation_logticks would throw that error.
    
    levelOrder is now also passed from plot_fluxxer regardless of whether it's actually sensible (e.g. if there's only one data point).

--- R/plot_mutrates.R
+++ R/plot_mutrates.R
@@ -1,18 +1,16 @@
 # core plotting function
 
-plot_mutrates <- function(data, levelOrder){
+plot_mutrates <- function(data, levelOrder, log = NULL){
   # ARGS:
   # data - the data to plot. probably always combined data
   # levelOrder - the levels to refactor by, including if reps are missing
   
-  # this might not work
-  if(is.null(levelOrder)){
-    x <- data$strain
-  } else {
-    x <- forcats::fct_relevel(strain, levelOrder)
-  }
+  # DEBUG
+  # data <- plotData$data
+  # levelOrder <- plotData$levels
   
-  plot <- ggplot(data, aes(x = x, 
+  # make most of the plot
+  plot <- ggplot(data, aes(x = forcats::fct_relevel(strain, levelOrder), 
                            y = mu)) +
     geom_point(shape = 16, size = 2, position = position_dodge(width = 0.75)) +
     geom_errorbar(aes(ymin=CI.95.lower, ymax=CI.95.higher), width = 0.15, linewidth = 0.4,
@@ -20,8 +18,12 @@ plot_mutrates <- function(data, levelOrder){
     scale_y_log10() +
     xlab(NULL) +
     ylab("Mutation rate") +
-    annotation_logticks(sides = "l") +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+  
+  # if this flag is true, logticks can be added and won't cause an error
+  if(isTRUE(log)){
+    plot <- plot + annotation_logticks(sides = "l")
+  }
   
   
   return(plot)
