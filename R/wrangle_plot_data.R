848a3cc9c001c6106471732e473e8e3433248824
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-02-01 16:09:51 +1000

    Changed wrangle_plot_data()
    Rewrote a large part of this function around the single and pipeline mode. The flow is now more consistent with the other pipeline functions.
    
    Modes are set by whether a project name or a file path is supplied; the former should imply plot_fluxxer() called it in pipeline mode, and it will scan the folder structure (whether or not there's >1 project is not important). In single file mode, no looping happens and the file is simply read in directly with no checks.
    
    Refactoring is now handled by refactor_reps(), and it's agnostic about whether levels are actually a sensible approach, because even if there's only one data point, goodOrder will work fine.
    
    Also implemented a check that the highest CI upper and the lowest CI lower are a difference of more than 1 power of 10. This is later passed as a flag to plot_mutrates().

--- R/wrangle_plot_data.R
+++ R/wrangle_plot_data.R
@@ -6,46 +6,64 @@
 
 ###
 
-wrangle_plot_data <- function(projectName, mode = NULL){
+wrangle_plot_data <- function(file = NULL, projectName = NULL){
   # ARGS:
   # projectName - each pair of pooled/unpooled data will have a prefix. Best approach is prolly
   #               to grep them somehow, since passing them explicitly is kinda weird.
   # mode - "single" or "standard", to be assigned explicitly by plot_flux(). "Single" will not
   #        have pooled/unpooled.
   
+  # DEBUG
+  
   ###
   
-  # Define dataPath (should be implied? unless this can be called outside of pipeline?)
-  dataPath <- "./output/analyzed"
-  
-  # Import unpooled data first
-  unpooledData <- read.csv(paste0(dataPath, "/", projectName, "_unpooled.output.csv"), header = T)
-  
-  # Extract replicate prefix (as long as the format is <prefix><number>)
-  reps <- unpooledData[, "strain"]
-  repPrefix <- unique(gsub("[[:digit:]]*$", "", reps))
-  if(length(repPrefix) > 1){
-    stop("One or more replicates are not named consistently.")
+  # Detect mode
+  if(!is.null(file) & is.null(projectName)){
+    runMode <- "single"
+  } else if(is.null(file) & !is.null(projectName)){
+    runMode <- "standard"
+  } else {
+    stop("That's odd, file XOR project should be NULL. Please report this bug.")
   }
   
-  # Automatically construct correct factor order for passing to plotter
-  idealOrder <- paste0(repPrefix, 1:1000)
-  badOrder <- unpooledData$strain
-  goodOrder <- c("Pooled", idealOrder[idealOrder %in% badOrder])
-  
-  if(mode == "single"){
+  # Begin wrangle
+  if(runMode == "single"){
+    
+    # Load data & refactor reps
+    singleData <- read.csv(file, header = T)
+    goodOrder <- refactor_reps(singleData)
+    
+    # Handle ggplot2::annotation_logticks() error condition
+    range <- c(singleData$CI.95.lower, singleData$CI.95.higher)
+    if(log(max(range)) - log(min(range)) >= 1){
+      logMode <- TRUE
+    } else {
+      logMode <- FALSE
+    }
+    # Note for later: 
+    # This is because annotation_logticks shits itself if max-min is less than one power of 10.
+    # If this doesn't work and it still throws an error, it might be because annotation_logticks()
+    # still doesn't like if max(range) - min(range) >= 1 if they don't actually span two logticks
+    # in the final plot. If that's the case, a more conservative range should be used, i.e.
+    # floor(max(range)) - ceiling(min(range)), which collapses the difference to zero in this case.
+    
     # Construct export object
-    exportObject <- list("data" = unpooledData, "levels" = goodOrder)
-  } else if(mode == "standard"){
+    exportObject <- list("data" = singleData, "levels" = goodOrder, "log" = logMode)
+   
+  } else if(runMode == "standard"){
+    
+    # Load data & refactor reps
+    unpooledData <- read.csv(paste0(dataPath, "/", projectName, suffix), header = T)
+    goodOrder <- refactor_reps(unpooledData)
+    
     # Import the pooled data and combine with unpooled
     pooledData <- read.csv(paste0(dataPath, "/", projectName, "_pooled.output.csv"), header = T)
     combinedData <- rbind(unpooledData, pooledData)
     combinedData$strain[which(combinedData$strain == "AB3")] <- "Pooled"
     
     # Construct export object
-    exportObject <- list("data" = combinedData, "levels" = goodOrder)
-  } else if(is.null(mode)){
-    stop("mode is NULL! This should not have happened, please report this bug.")
+    exportObject <- list("data" = combinedData, "levels" = goodOrder, "log" = logMode)
+    
   }
   
   
