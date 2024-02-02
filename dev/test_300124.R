5f58554ddf8440cfe27c364c2f05f6b77bc3c9f2
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-02-02 16:16:15 +1000

    Updated test
    Testing got up to the plotting stage, but now that I've changed a bunch of functions I'll have to test from the beginning again...

--- dev/test_300124.R
+++ dev/test_300124.R
@@ -13,12 +13,20 @@ source("./R/wrangle_clean_data.R")
 testRaw <- ("./data/testing 2024 01 30/test_raw.xlsx")
 testWrangled <- ("./data/testing 2024 01 30/test_wrangled.csv")
 
-wrangle_raw_data(testRaw, 4)
-wrangle_clean_data(testWrangled, saveAs = "EL_test")
+wrangle_raw_data(testRaw, 4, overwrite = TRUE)
+wrangle_clean_data(testWrangled, saveAs = "EL_test", overwrite = TRUE)
 
 # Part 2: Analyze
 source("./R/run_fluxxer.R")
 source("./R/calculate_mut_rate.R")
 
-run_fluxxer()
+run_fluxxer(overwrite = TRUE)
 
+# Part 3: Plot
+source("./R/plot_fluxxer.R")
+source("./R/wrangle_plot_data.R")
+source("./R/refactor_reps.R")
+source("./R/plot_mutrates.R")
+source("./R/export_mut_plot.R")
+
+plot_fluxxer()
