f7e2aa2a7368f8dee7082b90d98c3f801af38c4f
pseudonoma <56713583+pseudonoma@users.noreply.github.com>
2024-02-02 16:06:50 +1000

    Changed export_mut_plot()
    Changed outputPath variable for consistency with how the other functions use it.

--- R/export_mut_plot.R
+++ R/export_mut_plot.R
@@ -1,9 +1,9 @@
 # helper function for exporting mutation rate plots
 
-export_mut_plot <- function(plot, project, exportPath){
+export_mut_plot <- function(plot, prefix, outputPath){
   
-  plotName <- paste0("plot_", project)
-  plotPath <- exportPath
+  plotName <- paste0("plot_", prefix)
+  exportPath <- outputPath # just for consistency
   ggsave(paste0(plotName, ".pdf"), plot = plot,
          height = 6, width = 8, units = "in", dpi = 300,
          limitsize = TRUE, path = exportPath)
