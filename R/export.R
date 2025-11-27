5625c10cb58cc77376d9fbb57221fd2659dadb9f
Ian G. <56713583+pseudonoma@users.noreply.github.com>
2025-11-27 10:26:53 +1000

    Modified result exporter functions
    In plot_fluxxer():
    - Removed hardcoded pathing to luria output folder; it still requires reimporting data files, but this is enforced by wrapper now.
    - Function now takes a single CSV file or a dir; if dir, tries to find project pairs to combine, or errors out.
    - Removed arg return.plots; now returns ggplot object by default.
    - Added arg export; optionally export plots with previous method.
    
    In extract_mutrates():
    - Changed arg method, added arg export.to
    - Removed hardcoded pathing
    - Now takes arg dataPath to */luria_output/analyzed/
    - Added errcatch if can't find pooled data in dataPath
    - Added ability to export CSV
    - Now returns df by default
    
    In get_template():
    - Renamed export args to match new pipeline standard
    - Modified save location message to be less ambiguous, but pathing still feels a bit fragile...

--- R/export.R
+++ R/export.R
@@ -1,97 +1,107 @@
-#' Plot mutation rate data.
+#' Plot mutation rate data
 #'
 #' Produce a dot plot of estimated mutation rates, with error bars representing upper and lower
 #' 95% confidence intervals.
 #'
 #' @details
-#' Like [`run_fluxxer()`], supplying the `file` argument will cause this function to run in single-
-#' file mode, plotting the data as-is. Otherwise, it will automatically read through the pipeline
-#' folder `./output/analyzed` and combine pairs of pooled and unpooled data into individual plots.
-#' Plots are saved as 6 inch by 8 inch images, in both PNG and PDF format, to `./output/analyzed`.
+#' This function can be supplied either a single CSV file produced by [`run_fluxxer()`] or a folder
+#' of pooled/unpooled CSVs produced by that method. In the latter case, matching pooled and unpooled
+#' data will be combined into a single plot showing the mutation rates pooled and by strain. Plots
+#' are saved as 6 inch by 8 inch images, in both PNG and PDF format, to `/luria_output/analyzed/`.
+#' Note that this function does not take dataframes.
 #'
 #' @inheritParams prep_export
-#' @param file The filename of an output file to plot mutation rates from.
-#' @param return.plots Logical. If `TRUE`, the plot is returned. This is useful if you would like to
-#' add further ggplot2 aesthetics, or display the plot in RStudio.
+#' @param dataPath A path to an output CSV file to plot mutation rates from. Alternatively, a folder
+#' path containing pooled/unpooled file pairs.
+#' @param export If `TRUE`, plots are exported as PDF and PNG files.
 #'
 #' @examples
-#' plot <- plot_fluxxer(file = "./data/analyzed/wrangled.output.csv",
-#'                      return.plots = TRUE,
+#' plot <- plot_fluxxer(dataPath = "./data/analyzed/wrangled.output.csv",
 #'                      overwrite = TRUE)
 #'
 #' @return
-#' If `return.plots` is `TRUE`, either a single `ggplot2` object (in single-file mode), or a list
-#' of `ggplot2` objects, each corresponding to a pair of pooled and unpooled input data.
+#' A single `ggplot2` object, or a list of `ggplot2` objects, each one corresponding to a pair of
+#' pooled and unpooled input data.
 #'
 #' @export
 
-plot_fluxxer <- function(file = NULL, return.plots = FALSE, overwrite = FALSE){
+plot_fluxxer <- function(dataPath, export = TRUE, overwrite = TRUE, export.to = "."){
 
-  # Set mode explicitly once again
-  runMode <- ifelse(is.null(file), yes = "standard", no = "single")
+  if(!is.character(dataPath)){
+    stop("This function wants a CSV file or a folder of such.")
+  }
 
-  # Prep export folder (in std mode it's redundant this time though)
-  outputPath <- prep_export(mode = "plots", overwrite)
+  # Prep export folder and handle overwriting
+  outputPath <- prep_export(mode = "plots", outputParent = export.to, overwrite)
 
-  ##### Single-file mode #####
-  if(runMode == "single"){
+  # Detect input type (single or folder)
+  if(is.character(dataPath)){
+    if(utils::file_test("-d", dataPath)){ # dataPath is dir
+      isDirectory <- TRUE
+    } else {
+      isDirectory <- FALSE
+    }
+  } else {
+    stop("This function wants a file or folder path as input.")
+  }
+
+  # Begin wrangle
+  if(isFALSE(isDirectory)){ # single file supplied
 
     # wrangle & plot
-    plotData <- wrangle_plot_data(file = file, projectName = NULL)
+    plotData <- wrangle_plot_data(file = dataPath)
     plot <- plot_mutrates(data = plotData$data, levelOrder = plotData$levels, log = plotData$log)
 
-    # grep "project name" file prefixes
-    prefix <- sub(".output.csv$", "", basename(file))
-
-    # export
-    export_mut_plot(plot, prefix, outputPath)
-    message("\nPlot saved to /analyzed/.")
-
-    # construct return object (unnecessary, but matches pipeline mode)
-    plots <- plot
-
-    ##### Standard pipeline mode #####
-  } else if(runMode == "standard"){
-
-    # Test for standard pipeline dirs
-    inputPath <- "./output/analyzed"
-    if(!dir.exists(inputPath)){
-      stop("Folder /wrangled/ not found. In standard mode, the pipeline must be run in order.")
+    # grep "project name" file prefix and export
+    prefix <- sub(".output.csv$", "", basename(dataPath))
+    if(export){
+      export_mut_plot(plot, prefix, outputPath)
+      message("Plot saved to /analyzed/.\n")
     }
 
+    # for return
+    plots <- plot
+
+  } else if(isDirectory){ # folder with filepairs
+
     # grep "project name" file prefixes
-    pooledList <- grep("_pooled.output.csv", dir(inputPath), value = TRUE)
+    # plotting is by project, so pooled files imply unpooled filenames too
+    pooledList <- grep("_pooled.output.csv", dir(dataPath), value = TRUE)
     projectList <- sub("_pooled.output.csv$", "", pooledList)
+
+    if(length(projectList) == 0){
+      stop("No pooled estimates found. If supplying a folder, it must have pooled/unpooled files.")
+    }
 
     # loop and plot over all projects
     plots <- list()
     for(projectName in projectList){
-      plotData <- wrangle_plot_data(file = NULL, projectName, inputPath)
+      plotData <- wrangle_plot_data(file = NULL, projectName, dataPath)
       plot <- plot_mutrates(data = plotData$data, levelOrder = plotData$levels, log = plotData$log)
 
       # export
-      export_mut_plot(plot, prefix = projectName, outputPath)
+      if(export){
+        export_mut_plot(plot, prefix = projectName, outputPath)
+      }
 
       # construct return object
       plots[[projectName]] <- plot
 
     }
 
-    message("\nPlots saved to /analyzed/.")
+    if(export){
+      message("Plots saved to /analyzed/.\n")
+    }
 
   }
 
 
-  if(return.plots){
-    return(plots)
-  } else {
-    return(invisible())
-  }
+  return(plots)
 
 }
 
 
-#' The core plotting function.
+#' The core plotting function
 #'
 #' Produce a ggplot object with or without a log-scale y-axis.
 #'
@@ -136,79 +146,96 @@ plot_mutrates <- function(data, levelOrder, log = NULL){
 }
 
 
-#' Extract mutation rates in a reproducible manner.
+#' Extract mutation rates in a reproducible manner
 #'
-#' Automatically get mutation rates from output data and save it as an RData object.
+#' Automatically get mutation rates from output data and save it.
 #'
 #' @details
-#' This function was originally designed to quickly export pooled mutation rate estimates for
-#' simulation work, making it easier to track estimate versions and removing the need for copy +
-#' pasting. It is likely only useful if you have multiple projects to extract numbers from.
-#' This function does not take single files, as it really only makes sense to take the pooled
-#' estimate anyway.
+#' Extracts pooled mutation rate estimates from multiple projects of pooled/unpooled file pairs
+#' into a vector named by project, and exports them as a CSV of project name and estimates, and/or
+#' an RData file. Likely only useful for extracting mutation rates from multiple projects at once.
+#' This function does not take single files, and specifically looks for pooled estimate data as it
+#' really only makes sense to use those.
 #'
 #' @inheritParams prep_export
-#' @param method Takes value `"save"`, which exports the data as an RData object; `"return"`, which
-#' returns the mutation rate as an RData object; or `"both"`, which does both.
+#' @param dataPath Path to a folder containing pooled mutation rates to extract.
+#' @param export.method Takes value `"csv"`, which exports the data as a CSV, `"data"`, which
+#' exports the data as an RData object; or `"all"`, which exports all formats.
 #'
 #' @examples
-#' mutrates <- extract_mutrates(method = "both")
+#' mutrates <- extract_mutrates(export.method = "all")
 #'
 #' @return
-#' If `method` is `"return"` or `"both"`, a numeric vector with elements named by project.
+#' A numeric vector with elements named by project.
 #'
 #' @export
 
-extract_mutrates <- function(method = "save", overwrite = FALSE){
+extract_mutrates <- function(dataPath, export.method = "all", overwrite = FALSE, export.to = "."){
 
   # Prep output
-  outputPath <- prep_export(mode = "mutrates", overwrite)
+  outputPath <- prep_export(mode = "mutrates", outputParent = export.to, overwrite)
+
+  # make sure input is a directory
+  if(!utils::file_test("-d", dataPath)){
+    stop("This function wants a folder path, not a file.")
+  }
 
   # Grep "project name" file prefixes
-  inputPath <- "./output/analyzed"
-  pooledList <- grep("_pooled.output.csv", dir(inputPath), value = TRUE)
+  pooledList <- grep("_pooled.output.csv", dir(dataPath), value = TRUE)
   projectList <- sub("_pooled.output.csv$", "", pooledList)
-
-  # Extract
-  mutRates <- c()
-  for(projectName in projectList){
-    pooledData <- read.csv(paste0(inputPath, "/", projectName, "_pooled.output.csv"), header = T)
-    mutRates[[projectName]] <- pooledData$mu
+  if(length(projectList) == 0){
+    stop("Could not find any pooled mutation rates to extract.")
   }
 
-  # Export
-  if(method == "save" | method == "both"){
-    timestamp <- format(Sys.Date(), "%Y-%m-%d")
-    save(mutRates, file = paste0(outputPath, "/", "mutrate_", timestamp, ".rda"))
-    message("\nMutation rate(s) RData object saved to /analyzed/.")
+  # Extract mutation rates
+  mutrateCSV <- data.frame(project = rep(NA, length(projectList)), mutation_rate = NA)
+  mutrateDat <- c()
+  for(i in 1:length(projectList)){
+    projectName <- projectList[i]
+    pooledData <- read.csv(paste0(dataPath, "/", projectName, "_pooled.output.csv"), header = T)
+    mutrateCSV$project[i] <- projectName
+    mutrateCSV$mutation_rate[i] <- pooledData$mu
+    mutrateDat[[projectName]] <- pooledData$mu
   }
-  if(method == "return" | method == "both"){
 
-
-    return(mutRates)
-
+  # Begin export
+  timestamp <- format(Sys.Date(), "%Y-%m-%d")
+  if(export.method == "csv" | export.method == "all"){
+    write.csv(mutrateCSV, file = paste0(outputPath, "/", "mutrate_", timestamp, ".csv"))
+    message("Mutation rate CSV saved to /analyzed/.")
   }
+  if(export.method == "data" | export.method == "all"){
+    save(mutrateDat, file = paste0(outputPath, "/", "mutrate_", timestamp, ".rda"))
+    message("Mutation rate RData file saved to /analyzed/.")
+  }
+
+
+  return(mutrateDat)
+
 }
 
 
-#' Save a copy of the fluctuation analysis data template.
+#' Save a copy of the fluctuation analysis data template
 #'
 #' Use this function to save a copy of the fluctuation analysis Excel data template.
 #'
-#' @param saveTo Path to save the template file. Defaults to the project root.
-#' @param saveAs Filename to use. The default filename is "template.xlsx".
 #' @param version Which version of the template to use. Defaults to `latest`; `old` saves a copy of
 #' the old template - use this at your own risk.
+#' @param save.as What the template file will be named. The default is "template".
+#' @param export.to Where to save the template file. Defaults to current directory.
 #'
 #' @examples
 #' get_template("./data", "my_data_template")
 #'
 #' @export
 
-get_template <- function(saveTo = ".", saveAs = "template", version = "latest"){
+get_template <- function(version = "latest", save.as = "template", export.to = "."){
 
-  # Doing this by reading in the file and saving it back out so I don't have to deal
-  # with low-level file manipulation across Windows and UNIX. Fuck that shit.
+  # NTS:
+  # Low-level file manipulation across both Windows and UNIX is hell, so just reading in
+  #   the file and saving it back out.
+  # This function doesn't really use the established helper tools in the rest of the pipeline.
+  # Exporting arguably should use new option in prep_export. Maybe later.
 
   # Read the file in
   if(version == "latest"){
@@ -219,12 +246,21 @@ get_template <- function(saveTo = ".", saveAs = "template", version = "latest"){
                                              package = "luria"))
   }
 
-  # Handle folders
-  dir.create(saveTo, recursive = TRUE)
+  # Export
+  # Create export dir; recursive creates missing dirs leading to export dir
+  dir.create(export.to, recursive = TRUE, showWarnings = FALSE)
+
+  # Handle savename
+  if(!is.null(save.as)){ # save.as supercedes all other names
+    exportName <- paste0(export.to, "/", save.as)
+  } else {
+    stop("save.as is NULL, how did this happen?")
+  }
 
   # Save the file back out
-  openxlsx::saveWorkbook(wb, paste0(saveTo, "/", saveAs, ".xlsx"))
-  message(paste0("Data template saved to ", saveTo))
+  exportPath <- paste0(exportName, ".xlsx")
+  openxlsx::saveWorkbook(wb, exportPath)
+  message(paste0("Data template saved to ", here::here(exportPath)))
 
 
   return(invisible())
